d666b1419889cf2fb5c741ac1282d874a736f7e6
wassim6 <falloussaf@gmail.com>
2015-10-09 06:33:22 +0100

    TestService commit

---
+++ airport/airport-ejb/src/main/java/edu/esprit/services/TestService.java
@@ -0,0 +1,20 @@
+package edu.esprit.services;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TestService
+ */
+@Stateless
+@LocalBean
+public class TestService implements TestServiceLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public TestService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
