2f0d9d8c8a3b7d0223fca932c848fe217f79009e
esprit <esprit@192.168.1.6>
2015-10-11 18:36:14 +0200

    CommitCommit

--- airport/airport-web/src/main/java/edu/esprit/ressources/ClientInfoRessource.java
+++ airport/airport-web/src/main/java/edu/esprit/ressources/ClientInfoRessource.java
@@ -2,12 +2,15 @@ package edu.esprit.ressources;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import edu.esprit.persistance.Client;
 import edu.esprit.persistance.ClientInfo;
 import edu.esprit.services.ClientInfoServiceLocal;
 
@@ -35,5 +38,23 @@ public class ClientInfoRessource {
 		return myejb.find(id);
 		
 	}
+	
+	@PUT
+    @Path("/update")
+	@Produces("application/json")
+    public void update(ClientInfo client)
+	{
+			 myejb.update(client);
+	}
+	
+	@DELETE
+	@Path("/delete")
+	@Consumes("application/json")
+	public void delete(ClientInfo client) {
+
+		 myejb.delete(client);
+
+	}
+	
 
 }
